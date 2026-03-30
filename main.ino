5cd76fdd2f9dfdee337ed6d8489576588d4948e2
TateMod <tate@modicafamily.co.uk>
2026-03-30 21:37:51 +0100

    Update main.ino
    comments and IMU filted

--- main.ino
+++ main.ino
@@ -38,16 +38,17 @@ bool imuAvailable = false;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);//serail input speed
 
 
-  pinMode(trigPin, OUTPUT );
+  pinMode(trigPin, OUTPUT );//if its outptuing somthing set output vicevera for input
   pinMode(echoPin, INPUT );
   pinMode(buzzPin, OUTPUT);
-  pinMode(joyButton, INPUT_PULLUP);
+  pinMode(joyButton, INPUT_PULLUP);// pullup resister pult in function to pull the line high to prevent noise
 
   lcd.begin(20, 4);
   lcd.print("Hello, World!");
+  //IMU intilistion eror handling
   IMU.begin();
     if (!IMU.begin()) {
       Serial.println("Failed to initialize IMU!");
@@ -63,19 +64,19 @@ void setup() {
 //void loop() {
   
   /*
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
-  delay(1000);
 
+  // for LCD
+  lcd.setCursor(0, 1); //sets location o teh LCD im usign it to set point where to add stuff
+  lcd.print(millis() / 1000); //adding timer at the  position (0,1) as set
+  delay(1000);
+  // this is teh ultra sonic sensor section
   // off (low) then sends plus (high) tehn stops sending pulse (low)
   digitalWrite(trigPin, LOW);
   delayMicroseconds(10);// 10 microseconds as sensors needs that
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
   long duration = pulseIn(echoPin, HIGH); // reads pulse
-
   int distance = duration * 0.034 / 2; //calulaes distance based on spped of sound and half cause only need distance 
 
   //Serial.print("distance: ");//printing distance
@@ -84,15 +85,15 @@ void setup() {
 */
   
 void loop() {
-  float ax, ay, az;
+  float ax, ay, az;//varabyles for string XYZ
   float gx, gy, gz;
-
+// reading IMUs gyro XYZ and accell XYZ
   if (IMU.accelerationAvailable()) IMU.readAcceleration(ax, ay, az);
   if (IMU.gyroscopeAvailable()) IMU.readGyroscope(gx, gy, gz);
-
+//getting angles
   float accel_pitch = atan2(ay, az) * 180 / PI;
   float accel_roll  = atan2(ax, az) * 180 / PI;
-
+//complementory filter
   pitch = alpha * (pitch + gx * dt) + (1 - alpha) * accel_pitch;
   roll  = alpha * (roll  + gy * dt) + (1 - alpha) * accel_roll;
 
