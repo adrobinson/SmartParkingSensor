f6d311b8a2a49f1afe175978d219045494efc3c0
TateMod <tate@modicafamily.co.uk>
2026-05-04 23:10:38 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,90 +1,158 @@
 #include <Arduino_LSM9DS1.h>
-#include <LiquidCrystal_74HC595.h>
 #include <math.h>
+#include <LiquidCrystal_74HC595.h>
 
-#define DS 11
-#define SHCP 13
-#define STCP 12
-#define RS 1
-#define E 2
-#define D4 3
-#define D5 4
-#define D6 5
-#define D7 6
+class ultraSonic{
+  private:
+    String name;
+    int trigPin;
+    int echoPin;
+    long duration;
+    int distance;
 
-LiquidCrystal_74HC595 lcd(DS, SHCP, STCP, RS, E, D4, D5, D6, D7);
+  public:
 
-int trigPin = 9;
-int echoPin = 12;
-int buzzPin = A3;
-int joyButton = 8;
-int joyY = A0;
-int joyX = A1;
+    ultraSonic(String name, int trig,int echo){
+      this->name = name;
+      trigPin= trig;
+      echoPin = echo;
+      pinMode(trigPin, OUTPUT);
+      pinMode(echoPin, INPUT);
 
-float pitch = 0;
-float roll = 0;
-float alpha = 0.98;
-float dt = 0.01;
+    }
 
-bool imuAvailable = false;
+  void read(){
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      duration = pulseIn(echoPin, HIGH, 30000);
+      distance = duration * 0.034 / 2;
+      delay(25);
+  }
+
+  void print(){
+    Serial.print(name + ": ");
+    Serial.println(duration  == 0 ? "Out of range" : String(distance)  + "cm");
+  }
+   int getDistance() { return distance; }
+};
+
+
+class LED{
+  private:
+    String name;
+    int LEDPin;
+  public:
+    LED(String name, int Pin){
+    this->name = name;
+    LEDPin = Pin;
+    pinMode(Pin, OUTPUT);
+    }
+    void on()  { digitalWrite(LEDPin, HIGH); } 
+    void off() { digitalWrite(LEDPin, LOW);  }  
+};
+
+
+class IMUClass{
+
+  private:
+    float ax, ay, az;
+    float gx, gy, gz;
+    float accel_pitch, accel_roll;
+    float pitch, roll;
+    float alpha;
+    unsigned long lastTime;
+    bool imuAvailable;
+  
+  public:
+  IMUClass(){
+      alpha = 0.98;
+      pitch = 0;
+      roll  = 0;
+      lastTime = 0;
+      imuAvailable = false;
+  }
+
+  void begin() {
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU!");
+        imuAvailable = false;
+        while (1);
+      }
+      imuAvailable = true;
+    }
+
+  void read() {
+    if (IMU.accelerationAvailable()) IMU.readAcceleration(ax, ay, az);
+    if (IMU.gyroscopeAvailable())    IMU.readGyroscope(gx, gy, gz);
+    }
+  void process(){
+    float dt = (millis() - lastTime) / 1000.0;
+    lastTime = millis();
+    accel_roll  = atan2(ay, az) * 180.0 / PI;
+    accel_pitch = atan2(-ax, sqrt(ay*ay+az*az)) * 180.0 / PI;
+//complemntory filter
+    pitch = alpha * (pitch + gx * dt) + (1 - alpha) * accel_pitch;
+    roll  = alpha * (roll  + gy * dt) + (1 - alpha) * accel_roll;
+  }
+
+  void printFiltered(){
+
+    Serial.print(pitch);       Serial.print(",");
+    Serial.println(roll);Serial.print(".");
+  }
+  void printRaw(){
+    Serial.print(accel_pitch); Serial.print(",");
+     Serial.print(accel_roll);  Serial.print(".");
+
+  }
+
+    float getPitch() { return pitch; }
+    float getRoll()  { return roll;  }
+
+
+};
+
+
+class LCDDisplay {
+  private:
+    LiquidCrystal_74HC595 lcd;
+  public:
+    LCDDisplay() : lcd(11, 13, 12, 1, 3, 4, 5, 6, 7) {}
+    void begin() { lcd.begin(20, 4); }
+    void print(String msg) { lcd.print(msg); }
+    void clear() { lcd.clear(); }
+};
+
+class Buzzer {
+
+  private:
+  int buzPin;
+
+  public:
+    Buzzer(int Pin){
+      buzPin = Pin;
+      pinMode(buzPin, OUTPUT);
+    }
+  void noise(){
+    digitalWrite(buzPin, HIGH);
+    }
+  void off(){
+    digitalWrite(buzPin, LOW);
+  }
+    
+
+
+};
 
 void setup() {
-  Serial.begin(9600);
+  // put your setup code here, to run once:
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(buzzPin, OUTPUT);
-  pinMode(joyButton, INPUT_PULLUP);
-
-  lcd.begin(20, 4);
-  lcd.print("Hello, World!");
-
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    imuAvailable = false;
-    while (1);
-  } else {
-    imuAvailable = true;
-  }
 }
 
 void loop() {
-  // --- Ultrasonic sensor ---
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
-  //delay(1000);
+  // put your main code here, to run repeatedly:
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);        // settle first
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  long duration = pulseIn(echoPin, HIGH, 30000); // 30ms timeout
-  int distance = duration * 0.034 / 2;
-
-  // imu
-  float ax, ay, az;
-  float gx, gy, gz;
-
-  if (IMU.accelerationAvailable()) IMU.readAcceleration(ax, ay, az); // mapping to x,y,z
-  if (IMU.gyroscopeAvailable()) IMU.readGyroscope(gx, gy, gz);
-
-  float accel_pitch = atan2(ay, az) * 180 / PI; // conver to angles
-  float accel_roll  = atan2(ax, az) * 180 / PI;
-// complemntory filter.
-  pitch = alpha * (pitch + gx * dt) + (1 - alpha) * accel_pitch;
-  roll  = alpha * (roll  + gy * dt) + (1 - alpha) * accel_roll;
-
-  Serial.print("Dist: "); 
-  Serial.print(distance); // prints ultranoice snroce distance estimatons
-  Serial.println("cm");
-
-  
-  Serial.print(accel_pitch); Serial.print(",");   
-  Serial.print(accel_roll);  Serial.print(",");
-  Serial.print(pitch);       Serial.print(",");
-  Serial.println(roll);
-
-  delay(100);
 }
